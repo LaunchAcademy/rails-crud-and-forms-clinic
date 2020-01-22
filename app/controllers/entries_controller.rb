b58ef8a255d83ff33e7a4be96cd36cd1c717fc20
Stephen Fang <safang99@gmail.com>
2020-01-22 16:56:37 -0500

    final code

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,3 +1,32 @@
 class EntriesController < ApplicationController
+  def show
+    @entry = Entry.find(params[:id])
+    @journal = @entry.journal
+  end
+
+  def new
+    @entry = Entry.new
+    @journal = Journal.find(params[:journal_id])
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    @journal = Journal.find(params[:journal_id])
+    @entry.journal = @journal
+    # @journal = @entry.journal
+
+    if @entry.save
+      flash[:notice] = "entry added"
+      redirect_to @journal
+    else
+      flash.now[:errors] = @entry.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+  def entry_params
+    params.require(:entry).permit(:title, :body)
+  end
 
 end
--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -6,16 +6,27 @@ class JournalsController < ApplicationController
 
   def show
     @journal = Journal.find(params[:id])
+    @entries = @journal.entries
   end
 
   def new
-    #
+    @journal = Journal.new
   end
 
   def create
-    # 
+    @journal = Journal.new(journal_params)
+
+    if @journal.save
+      flash[:notice] = "Journal created"
+      redirect_to @journal
+    else
+      flash.now[:error] = @journal.errors.full_messages.to_sentence
+      render :new
+    end
   end
 
   private
-
+  def journal_params
+    params.require(:journal).permit(:name, :year, :author, :description, :theme)
+  end
 end
