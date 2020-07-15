81fe30de3c758e576611bb8abf4ed965c5b0db2c
nwalberts <nwalberts5@gmail.com>
2020-07-15 14:31:50 -0400

    done

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,3 +1,40 @@
 class EntriesController < ApplicationController
 
+  def index 
+    @entries = Entry.all
+  end 
+
+  def new 
+    @journal = Journal.find(params[:journal_id])
+    @entry = Entry.new
+  end
+
+  def create 
+    @journal = Journal.find(params[:journal_id])
+    @entry = Entry.new(entry_params)
+    @entry.journal = @journal
+
+    if @entry.save 
+      flash[:notice] = "YAY PUBERTY"
+      redirect_to @journal
+    else  
+      flash[:notice] = @entry.errors.full_messages.to_sentence
+      render :new
+    end
+
+  end
+
+  def show 
+    @entry = Entry.find(params[:id])
+    @journal = @entry.journal
+  end
+
+  private 
+
+  def entry_params 
+    params.require(:entry).permit(:title, :body)
+  end
+
+
+
 end
--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -2,20 +2,34 @@ class JournalsController < ApplicationController
 
   def index
     @journals = Journal.all
+    # render :index
   end
 
   def show
     @journal = Journal.find(params[:id])
+    @entries = @journal.entries
   end
 
   def new
-    #
+    @new_journal = Journal.new
   end
 
   def create
-    # 
+    @new_journal=  Journal.new(journal_params)
+    if @new_journal.save
+      flash[:notice] = "Omg new journal, so many juicy secrets."
+      # redirect "/journals/#{@new_journal.id}"
+      redirect_to @new_journal
+    else 
+      flash[:notice] = @new_journal.errors.full_messages.to_sentence
+      render :new
+    end
   end
 
   private
+
+  def journal_params 
+    params.require(:journal).permit(:name, :year, :author, :description, :theme)
+  end
 
 end
