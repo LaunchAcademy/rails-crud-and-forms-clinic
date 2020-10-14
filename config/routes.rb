d83fcb034d61adb23ead4abd977bd9bc4c43022a
dovakeen118 <kegillis118@gmail.com>
2020-10-14 12:10:45 -0400

    update starter code

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "journals#index"
 
-  resources :journals, only: [:index, :show, :new]
+  resources :journals, only: [:index, :show]
 
 end
